66a02763c3c4aa97675d29cff983e85516808b05
Дмитрий <Dkrovel@gmail.com>
2019-09-02 23:03:21 +0800

    Adaptive burger menu (#1)
    * Deleted unwanted lines
    
    * Added :hover and :active for burgermenu
    
    * Some fix lg burger version
    
    * Created 914px/420px version burger

--- src/js/script.js
+++ src/js/script.js
@@ -8,11 +8,9 @@ navbar.onclick = () =>{
     if(navbar.classList.contains('active')){
         navbar.classList.remove("active");
         Menu.style.maxHeight = "0"
-        Menu.style.transition= "0.3s all ease-out";
     }
     else{
         navbar.classList.add("active");
         Menu.style.maxHeight = "720px";
-        Menu.style.transition= "0.7s all ease-in";
     }
 }
